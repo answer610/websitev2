10234cd079147f1cdcc70f1b018b8adefdd6e4a6
answer610 <anhongle2002@gmail.com>
2022-12-09 22:40:42 -0800

    Add blank tags to links

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -15,10 +15,10 @@ export default class About extends Component {
                     alignItems: "center",
                     marginTop: "10px"
                 }}>
-                    <SocialIcon url="https://www.linkedin.com/in/an-le-048523197/"/> &nbsp;
-                    <SocialIcon url="https://drive.google.com/file/d/1-I8fEIiv5w5vEmp2f9K8eZU3tn4yqaKg/view?usp=sharing"/> &nbsp;
+                    <SocialIcon url="https://www.linkedin.com/in/an-le-048523197/" target = "_blank"/> &nbsp;
+                    <SocialIcon url="https://drive.google.com/file/d/1-I8fEIiv5w5vEmp2f9K8eZU3tn4yqaKg/view?usp=sharing" target = "_blank"/> &nbsp;
                     <SocialIcon url="mailto:anl2002@berkeley.edu"/> &nbsp;
-                    <SocialIcon url="https://github.com/answer610" fgColor="white" bgColor="black"/> 
+                    <SocialIcon url="https://github.com/answer610" fgColor="white" bgColor="black" target="_blank"/>
                 </span>
                 <span className="about">
                     <p>Hi! My name is An Le and I am a undergraduate student at UC Berkeley pursuing a major in Computer Science. I am not 100% sure what
